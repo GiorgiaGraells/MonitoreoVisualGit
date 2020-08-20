71461832b26725364bda6ad0de90cb78f921cfca
GiorgiaGraells <gygraell@gmail.com>
2020-08-20 16:34:38 -0400

    Grafico de riqueza de especies por ambiente y estacion Limpieza de script vegan

--- AnalisisVegan.R
+++ AnalisisVegan.R
@@ -1,6 +1,6 @@
 #Vegan 
 
-#Exploración datos y analisis preliiminares
+#Exploración datos y analisis preliminares
 #PAQUETE VEGAN
 
 library(readxl)
@@ -10,27 +10,33 @@ library(MASS)
 library(ggplot2)
 library(knitr)
 
-AvesInv <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/Muestreo Aves jun-jul 2019/Monitoreo_punto/Reg_completo_aves.csv") %>% dplyr::mutate(Especie = str_trim(str_remove_all(Especie,pattern = "JUVENIL")))
-AvesPrim <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/Muestreo aves sep-oct 2019/Monitoreo punto/Registro_aves_veranoFINAL.csv")
+#Preparación de datos
 
-###### Modificando data frame
-
+AvesInv <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Muestreo aves jun-jul 2019/Monitoreo_punto/Reg_completo_aves_inv.csv") %>% dplyr::mutate(Especie = str_trim(str_remove_all(Especie,pattern = "JUVENIL")))
 AvesInv <- AvesInv %>% group_by(Sitio, Especie) %>% summarise(n=sum(N_individuos)) %>% spread(key = Especie, value = n, fill=0) %>% ungroup
-rownames(AvesInv) <-AvesInv$Sitio
 AvesInv <- AvesInv %>% dplyr::select(-Sitio)
 
+AvesPrim <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Muestreo aves sep-oct 2019/Monitoreo punto/Registro_aves_veranoFINAL.csv")
 AvesPrim <- AvesPrim %>% group_by(Sitio, Especie) %>% summarise(n=sum(N_individuos)) %>% spread(key = Especie, value = n, fill=0) %>% ungroup
-rownames(AvesPrim) <-AvesPrim$Sitio
-AvesPrim <- AvesPrim %>% dplyr::select(-Sitio)
+AvesPrim <-AvesPrim %>% dplyr::select(-Sitio)
 
-Amb<- read_rds("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/Analisis_Occu_punto/Occdata_ocu.rds")
-Sitio_Amb <- Amb %>% dplyr::select(Sitio, AMBIENTE)
+#Datos ambientales  incluidos buffers
+Amb<- read_rds("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Occdata_occu.rds")
+Amb <- Amb %>% dplyr::select(-Sitio)
 
-#Inv_amb <- AvesInv %>% left_join(Sitio_Amb) %>% group_by(AMBIENTE, Especie) %>% summarise(n=sum(N_individuos))%>% spread(key = Especie, value = n, fill=0) %>% ungroup
-#Prim_amb <- AvesPrim %>% left_join(Sitio_Amb) %>% group_by(AMBIENTE, Especie) %>% summarise(n=sum(N_individuos))%>% spread(key = Especie, value = n, fill=0) %>% ungroup
 
-#write.csv(Inv_amb, "AvesInv_porAmb.csv")
-#write.csv(Prim_amb, "AvesPrim_porAmb.csv")
+#############
+#matriz de similitudes
+#Invierno
+Inv.dist <- vegdist(AvesInv)
+Inv.ano <- with(Amb, anosim(Inv.dist, AMBIENTE))
+summary(Inv.ano)
+plot(Inv.ano, xlab="Clases", ylab="Ranking de distancias")
+
+Prim.dist <- vegdist(AvesPrim)
+Prim.ano <- with(Amb, anosim(Prim.dist, AMBIENTE))
+summary(Prim.ano)
+plot(Prim.ano, xlab="Clases", ylab="Ranking de distancias")
 
 
 ############inspeccion datos
--- Exploración_enfocada.R
+++ Exploración_enfocada.R
@@ -11,28 +11,57 @@ library(knitr)
 
 #Preparacion datos
 
-
-BirdNames <- read_rds("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/BirdFunc.rds")%>% mutate(Especie=str_replace_all(Especie, " ", "_"))
-BirdNames <- BirdNames %>%  dplyr::select(Nombre, Especie) %>%  rename(cientifico =Especie) %>%   rename(Especie =Nombre) 
-
-AvesInv <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Muestreo aves jun-jul 2019/Monitoreo_punto/Reg_completo_aves_inv.csv") %>% dplyr::mutate(Especie = str_trim(str_remove_all(Especie,pattern = "JUVENIL")))
-AvesInv <- AvesInv %>% group_by(Sitio, Especie) %>% summarise(n=sum(N_individuos)) %>% 
-  mutate(Especie= str_replace(Especie, "PELICANO", "PELICANO COMÚN"),
-         Especie= str_replace(Especie, "JOTE NEGRO", "JOTE CABEZA NEGRA"),
-         Especie= str_replace(Especie, "LILEN", "LILE"))
-
-AvesInv <- left_join(x=AvesInv, y= BirdNames)
+BirdNames <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/BirdNames.csv") %>% dplyr::select(-X1)
 
 
+#Riqueza invierno
+AvesInv <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Muestreo aves jun-jul 2019/Monitoreo_punto/Reg_completo_aves_inv.csv") %>% 
+  dplyr::mutate(Especie = str_trim(str_remove_all(Especie,pattern = "JUVENIL")))%>% rename(Nombre =Especie)
+AvesInv <- AvesInv %>% group_by(Sitio, Nombre, AMBIENTE) %>% summarise(n=sum(N_individuos)) %>% 
+  mutate(Nombre= str_replace(Nombre, "PELICANO", "PELICANO COMÚN"),
+         Nombre= str_replace(Nombre, "JOTE NEGRO", "JOTE CABEZA NEGRA"),
+         Nombre= str_replace(Nombre, "LILEN", "LILE"))
+
+AvesInv <- left_join(x=AvesInv, y= BirdNames) 
+
+#Grafico riqueza especies invierno
+AvesInv_riq <- AvesInv %>% group_by(Sitio, AMBIENTE, Habitat) %>% summarise(Riqueza = n())
+AvesInv_riq <- AvesInv_riq %>% mutate(AMBIENTE=fct_relevel(AMBIENTE, "URBANO","VERDE", "ROQUERIO INTERVENIDO","PLAYA INTERVENIDA",  "ROQUERIO NATURAL"))
+
+Riqueza_Prom <- AvesInv_riq %>% group_by(AMBIENTE, Habitat) %>% summarise(Riqueza = mean(Riqueza))
+
+ggplot(AvesInv_riq, aes(x=Sitio, y=Riqueza)) + geom_col(aes(fill=Habitat), position="dodge") + facet_wrap(~AMBIENTE, scales = "free_x", ncol=2)+
+  scale_fill_manual(aesthetics = c("fill", "color"),values = c( '#5ab4ac','#d8b365')) +  theme_bw()+ ylab("Riqueza de especies invierno")+
+  xlab("Sitios de muestreo")+   theme(axis.text.x=element_blank()) + geom_hline(data = Riqueza_Prom,aes(yintercept = Riqueza, color = Habitat), lty=2)
 
 
 
-...
+# Riqueza primavera
 
+AMB_nombres <-read_rds("Occdata_occu.rds") %>% dplyr::select(Sitio, AMBIENTE)
 
-AvesPrim <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Muestreo aves sep-oct 2019/Monitoreo punto/Registro_aves_veranoFINAL.csv")
-AvesPrim <- AvesPrim %>% group_by(Sitio, Especie) %>% summarise(n=sum(N_individuos)) %>% spread(key = Especie, value = n, fill=0) %>% ungroup
-AvesPrim <-AvesPrim %>% dplyr::select(-Sitio)
+AvesPrim <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Muestreo aves sep-oct 2019/Monitoreo punto/Registro_aves_veranoFINAL.csv")%>% 
+  rename(Nombre =Especie) 
+AvesPrim <- left_join(AvesPrim, AMB_nombres)
+AvesPrim <- AvesPrim %>% group_by(Sitio, Nombre, AMBIENTE) %>% summarise(n=sum(N_individuos)) %>% 
+  mutate(Nombre= str_replace(Nombre, "PELICANO", "PELICANO COMÚN"),
+         Nombre= str_replace(Nombre, "JOTE NEGRO", "JOTE CABEZA NEGRA"),
+         Nombre= str_replace(Nombre, "LILEN", "LILE"),
+         Nombre= str_replace(Nombre, "CAHUIL", "GAVIOTA CAHUIL")) %>% 
+  mutate(AMBIENTE= str_replace(AMBIENTE, "ROCA INTERVENIDA", "ROQUERIO INTERVENIDO"),
+         AMBIENTE= str_replace(AMBIENTE, "ROCA NATURAL", "ROQUERIO NATURAL"))
+
+AvesPrim <- left_join(x=AvesPrim, y= BirdNames) 
+
+#Grafico riqueza especies primavera
+AvesPrim_riq <- AvesPrim %>% group_by(Sitio, AMBIENTE, Habitat) %>% summarise(Riqueza = n())
+AvesPrim_riq <- AvesPrim_riq %>% mutate(AMBIENTE=fct_relevel(AMBIENTE, "URBANO","VERDE", "ROQUERIO INTERVENIDO","PLAYA INTERVENIDA",  "ROQUERIO NATURAL"))
+
+Riqueza_Prom <- AvesPrim_riq %>% group_by(AMBIENTE, Habitat) %>% summarise(Riqueza = mean(Riqueza))
+
+ggplot(AvesPrim_riq, aes(x=Sitio, y=Riqueza)) + geom_col(aes(fill=Habitat), position="dodge") + facet_wrap(~AMBIENTE, scales = "free_x", ncol=2)+
+  scale_fill_manual(aesthetics = c("fill", "color"),values = c( '#5ab4ac','#d8b365')) +  theme_bw()+ ylab("Riqueza de especies primavera")+
+  xlab("Sitios de muestreo")+ theme(axis.text.x=element_blank()) + geom_hline(data = Riqueza_Prom,aes(yintercept = Riqueza, color = Habitat), lty=2)
 
 
 
