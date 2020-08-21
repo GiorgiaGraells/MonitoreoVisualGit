c28c4506bf0d40d096045e4f736232a43e7e01ed
GiorgiaGraells <gygraell@gmail.com>
2020-08-21 14:36:41 -0400

    Analisis NMDS, anosim, simper, envfit listos para abundancia, comp especies y ocupancia

--- Exploración_enfocada.R
+++ Exploración_enfocada.R
@@ -13,7 +13,7 @@ library(knitr)
 
 #####################################
 # PRIMERA PARTE: RIQUEZA DE ESPECIES
-
+#####################################
 
 #Preparacion datos
 BirdNames <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/BirdNames.csv") %>% dplyr::select(-X1)
@@ -66,11 +66,13 @@ ggplot(AvesPrim_riq, aes(x=Sitio, y=Riqueza)) + geom_col(aes(fill=Habitat), posi
   xlab("Sitios de muestreo")+ theme(axis.text.x=element_blank()) + geom_hline(data = Riqueza_Prom,aes(yintercept = Riqueza, color = Habitat), lty=2)
 
 
-#########################
-## SEGUNDA PARTE: ABUNDANCIA
 
 
-#preparacion de datos bundancia por especie
+#####################################
+# SEGUNDA PARTE: ORDINATION ANALISIS- para presencia sp, abundancias y prob ocupancia
+#####################################
+
+#preparacion de datos 
 
 AvesInv <- read_csv("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Muestreo aves jun-jul 2019/Monitoreo_punto/Reg_completo_aves_inv.csv") %>% dplyr::mutate(Especie = str_trim(str_remove_all(Especie,pattern = "JUVENIL")))
 AvesInv <- AvesInv %>% group_by(Sitio, Especie) %>% summarise(n=sum(N_individuos)) %>% spread(key = Especie, value = n, fill=0) %>% ungroup
@@ -92,13 +94,13 @@ Amb <- Amb %>% dplyr::select(-Sitio)%>%
   rename( `Plantacion de arboles` =`Buffer_2200_Plantación de árboles`)
   
 
+#############################################################################################################################################
+# PRES-AUS: En base a funcion metaMDS, que estandariza las abundancias, considera solo composicion de especies
 
+##### METAMDS Non-metric Multidimensional scaling- NMDS
 
-
-#### Non-metric Multidimensional scaling- NMDS
 #Invierno
-
-ord1Inv <- metaMDS(AvesInv) #stress= 0.2316387
+ord1Inv <- metaMDS(AvesInv) #stress= 0.234609 
 stressplot(ord1Inv)
 
 ordiplot(ord1Inv)
@@ -110,9 +112,7 @@ AnosimInv <-anosim(AvesInv, grouping=Amb$AMBIENTE, permutations = 999, distance 
 summary(AnosimInv)
 plot(AnosimInv)
 
-
 ##Primavera
-
 ord1Prim <- metaMDS(AvesPrim) #stress= 0.1947774
 stressplot(ord1Prim)
 
@@ -126,8 +126,7 @@ summary(AnosimPrim)
 plot(AnosimPrim)
 
 
-
-####SIMPER:gives the contribution of each species to overall dissimilarities, but these
+##### SIMPER:gives the contribution of each species to overall dissimilarities, but these
 #          are caused by variation in species abundances, and only partly by differences among groups.
 
 #  The function displays most important species for each pair of groups. These species contribute at
@@ -140,7 +139,7 @@ SimperPrim <- simper(AvesPrim, group=Amb$AMBIENTE)
 summary(SimperPrim)
 
 
-### envit: Fits an Environmental Vector or Factor onto an Ordination
+##### ENVIT: Fits an Environmental Vector or Factor onto an Ordination
 
 EnvfitInv <- envfit(ord=ord1Inv, env=Amb, permutations = 999, strata = NULL)
 plot(ord1Inv)
@@ -152,3 +151,150 @@ EnvfitPrim<- envfit(ord=ord1Prim, env=Amb, permutations = 999, strata = NULL)
 plot(ord1Prim)
 plot(EnvfitPrim)
 ordihull(ord1Prim, groups = Amb$AMBIENTE)
+
+
+EnvfitPrim <- envfit(ord1Prim~ AMBIENTE+`Cobertura vegetal`+ `Distancia a río`+Altura+ `Bosque nativo`+ Cultivos+ Grava + Oceano +Pastizales+ Matorrales+`Superficies impermeables` + `Suelo arenoso`+ `Plantacion de arboles`, data=Amb, perm=999)
+EnvfitPrim
+
+
+#############################################################################################################################################
+# ABUNDANCIAS: Modificacion de funcion metaMDS para considderar abundancias de especies por sitio
+
+##### METAMDS: Non-metric Multidimensional scaling- NMDS
+
+#Invierno
+ord2Inv <- metaMDS(AvesInv,autotransform = FALSE) #stress= 0.1325971
+stressplot(ord2Inv)
+
+ordiplot(ord2Inv)
+ordihull(ord2Inv,groups = Amb$AMBIENTE)
+points(ord2Inv)
+
+AnosimInv <-anosim(AvesInv, grouping=Amb$AMBIENTE, permutations = 999, distance = "bray", strata = NULL,
+                   parallel = getOption("mc.cores"))
+summary(AnosimInv)
+plot(AnosimInv)
+
+
+##Primavera
+ord2Prim <- metaMDS(AvesPrim,autotransform = FALSE) #stress= 0.0.1566688 
+stressplot(ord2Prim)
+
+ordiplot(ord2Prim)
+ordihull(ord2Prim, groups = Amb$AMBIENTE)
+points(ord2Prim)
+
+AnosimPrim <-anosim(AvesPrim, grouping=Amb$AMBIENTE, permutations = 999, distance = "bray", strata = NULL,
+                    parallel = getOption("mc.cores"))
+summary(AnosimPrim)
+plot(AnosimPrim)
+
+
+
+##### SIMPER:gives the contribution of each species to overall dissimilarities, but these
+#          are caused by variation in species abundances, and only partly by differences among groups.
+
+#  The function displays most important species for each pair of groups. These species contribute at
+#  least to 70 % of the differences between groups.
+
+SimperInv <- simper(AvesInv, group = Amb$AMBIENTE)
+summary(SimperInv, ordered = TRUE)
+
+SimperPrim <- simper(AvesPrim, group=Amb$AMBIENTE)
+summary(SimperPrim)
+
+
+##### ENVFIT: Fits an Environmental Vector or Factor onto an Ordination
+
+EnvfitInv <- envfit(ord=ord2Inv, env=Amb, permutations = 999, strata = NULL)
+plot(ord2Inv)
+plot(EnvfitInv)
+ordihull(ord2Inv, groups = Amb$AMBIENTE)
+
+EnvfitInv <- envfit(ord2Inv~ AMBIENTE+`Cobertura vegetal`+ `Distancia a río`+Altura+ `Bosque nativo`+ Cultivos+ Grava + Oceano +Pastizales+ Matorrales+`Superficies impermeables` + `Suelo arenoso`+ `Plantacion de arboles`, data=Amb, perm=999)
+EnvfitInv
+
+EnvfitPrim<- envfit(ord=ord2Prim, env=Amb, permutations = 999, strata = NULL)
+plot(ord2Prim)
+plot(EnvfitPrim)
+ordihull(ord2Prim, groups = Amb$AMBIENTE)
+
+
+EnvfitPrim <- envfit(ord2Prim~ AMBIENTE+`Cobertura vegetal`+ `Distancia a río`+Altura+ `Bosque nativo`+ Cultivos+ Grava + Oceano +Pastizales+ Matorrales+`Superficies impermeables` + `Suelo arenoso`+ `Plantacion de arboles`, data=Amb, perm=999)
+EnvfitPrim
+
+
+#############################################################################################################################################
+# OCUPANCIAS: ocupando probabilidad de presencia por sitio
+
+#preparacion de datos
+
+PorSitioInv <- readRDS("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/PorSitioInv.rds") %>% 
+  dplyr::select(-Ambiente) %>% dplyr::select(-Sitio)
+PorSitioPrim <- readRDS("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/PorSitioPrim.rds") %>%
+  dplyr::select(-Ambiente) %>% dplyr::select(-Sitio)
+
+
+##### METAMDS: Non-metric Multidimensional scaling- NMDS
+
+#Invierno
+ord3Inv <- metaMDS(PorSitioInv, autotransform = FALSE) #stress= 0.2183811
+stressplot(ord3Inv)
+
+ordiplot(ord3Inv)
+ordihull(ord3Inv,groups = Amb$AMBIENTE)
+points(ord3Inv)
+
+AnosimInv <-anosim(PorSitioInv, grouping=Amb$AMBIENTE, permutations = 999, distance = "bray", strata = NULL,
+                   parallel = getOption("mc.cores"))
+summary(AnosimInv)
+plot(AnosimInv)
+
+
+##Primavera
+ord3Prim <- metaMDS(PorSitioPrim,autotransform = FALSE) #stress= 0.2280575 
+stressplot(ord3Prim)
+
+ordiplot(ord3Prim)
+ordihull(ord3Prim, groups = Amb$AMBIENTE)
+points(ord3Prim)
+
+AnosimPrim <-anosim(PorSitioPrim, grouping=Amb$AMBIENTE, permutations = 999, distance = "bray", strata = NULL,
+                    parallel = getOption("mc.cores"))
+summary(AnosimPrim)
+plot(AnosimPrim)
+
+
+
+##### SIMPER:gives the contribution of each species to overall dissimilarities, but these
+#          are caused by variation in species abundances, and only partly by differences among groups.
+
+#  The function displays most important species for each pair of groups. These species contribute at
+#  least to 70 % of the differences between groups.
+
+SimperInv <- simper(PorSitioInv, group = Amb$AMBIENTE)
+summary(SimperInv, ordered = TRUE)
+
+SimperPrim <- simper(PorSitioPrim, group=Amb$AMBIENTE)
+summary(SimperPrim)
+
+
+##### ENVFIT: Fits an Environmental Vector or Factor onto an Ordination
+
+EnvfitInv <- envfit(ord=ord3Inv, env=Amb, permutations = 999, strata = NULL)
+plot(ord3Inv)
+plot(EnvfitInv)
+ordihull(ord3Inv, groups = Amb$AMBIENTE)
+
+EnvfitInv <- envfit(ord3Inv~ AMBIENTE+`Cobertura vegetal`+ `Distancia a río`+Altura+ `Bosque nativo`+ Cultivos+ Grava + Oceano +Pastizales+ Matorrales+`Superficies impermeables` + `Suelo arenoso`+ `Plantacion de arboles`, data=Amb, perm=999)
+EnvfitInv
+
+EnvfitPrim<- envfit(ord=ord3Prim, env=Amb, permutations = 999, strata = NULL)
+plot(ord3Prim)
+plot(EnvfitPrim)
+ordihull(ord3Prim, groups = Amb$AMBIENTE)
+
+
+EnvfitPrim <- envfit(ord3Prim~ AMBIENTE+`Cobertura vegetal`+ `Distancia a río`+Altura+ `Bosque nativo`+ Cultivos+ Grava + Oceano +Pastizales+ Matorrales+`Superficies impermeables` + `Suelo arenoso`+ `Plantacion de arboles`, data=Amb, perm=999)
+EnvfitPrim
+
