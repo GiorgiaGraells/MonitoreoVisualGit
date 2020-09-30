d74497ce67b77d267960f892dc7e98f1290ee6db
GiorgiaGraells <gygraell@gmail.com>
2020-09-30 13:26:55 -0300

    Correccion de script utilizando los datos arreglados de deteccion. Se corrió acopancia y se corrigió la abundancia a partir de la deteccion para invierrno y prim

--- CorrecDetec_Abundancias.R
+++ CorrecDetec_Abundancias.R
@@ -6,6 +6,8 @@ library(unmarked)
 library(MuMIn)
 library(caret)
 
+
+####DATOS PRIMAVERA
 #Ocupancia para todas las especies en primavera
 
 data_det <-read_rds("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Analisis_Occu_punto/Occdata_detPrim.rds")
@@ -15,7 +17,6 @@ data_ocu <- data_ocu %>% dplyr::select(-Sitio)
 colnames(data_ocu) <- str_replace_all(colnames(data_ocu), pattern = " ", "_")
 
 data_reg <-read_rds("Occdata_regPRIM.rds")
-
 
 batchoccu2 <- function(pres, sitecov, obscov, spp, form, SppNames = NULL, dredge = FALSE) {
   if(is.null(SppNames)){
@@ -159,7 +160,6 @@ Spp <- data_reg %>% colnames() %>% str_remove_all("1")%>% str_remove_all("2") %>
 
 DetPrim <- batchoccu2(pres = data_reg, sitecov = data_ocu, obscov = data_det, spp=48,  form= "~ Temperatura +Humedad+ DirViento +RapViento+ Agua ~ 1", dredge=TRUE, SppNames = Spp)
 
-
 Detecciones <- list()
 
 for(i in 1:length(DetPrim$models)){
@@ -168,10 +168,9 @@ for(i in 1:length(DetPrim$models)){
 }
 
 
-
 Detecciones <- Detecciones %>% reduce(bind_cols)
 
-rownames(Detecciones) <- rownames(AbundPrim_dia)
+rownames(Detecciones) <- rownames(data_reg)
 
 ### Correccion de abundancias
 
@@ -179,3 +178,178 @@ AbundPrim_dia <-read_rds("Occdata_regPRIM_abund.rds")
 
 AbundPrim_Corregido <- AbundPrim_dia/Detecciones
 saveRDS(AbundPrim_Corregido, "AbundPrim_Corregido.rds")
+################################################################
+
+####DATOS INVIERNO
+#Ocupancia para todas las especies en invierno
+
+data_det <-read_rds("/home/giorgia/Documents/Doctorado tesis/Monitoreo aves/MonitoreoVisualGit/Analisis_Occu_punto/Occdata_detInv.rds")
+
+data_ocu <-read_rds("Occdata_occu.rds")
+data_ocu <- data_ocu %>% dplyr::select(-Sitio)
+colnames(data_ocu) <- str_replace_all(colnames(data_ocu), pattern = " ", "_")
+
+data_reg <-read_rds("Occdata_regInv.rds")
+
+
+batchoccu2 <- function(pres, sitecov, obscov, spp, form, SppNames = NULL, dredge = FALSE) {
+  if(is.null(SppNames)){
+    SppNames <- paste("species", 1:spp, sep =".")
+  }
+  secuencia <- c(1:spp)*(ncol(pres)/spp)
+  secuencia2<-secuencia-(secuencia[1]-1)
+  models <- vector('list', spp)
+  fit <- matrix(NA, nrow(pres), spp)
+  Mods <- list()
+  if(is.null(SppNames)){
+    colnames(fit) <- paste("species", 1:spp, sep =".")
+  }else if(class(SppNames) == "character"){
+    colnames(fit) <- SppNames
+  }
+  if (dredge == FALSE) {
+    for(i in 1:length(secuencia)) {
+      data <- pres[, secuencia2[i]:secuencia[i]]
+      data2 <- unmarkedFrameOccu(y = data, siteCovs = sitecov, obsCovs = obscov)
+      try({
+        models[[i]] <- occu(as.formula(form), data2)
+      }, silent = T)
+      try({
+        fit[, i] <- suppressWarnings(predict(models[[i]], type = "state", newdata = sitecov))$Predicted
+      }, silent = T)
+      Mods = NULL
+      print(paste("Species", as.character(i), "ready!"))
+    }
+  }
+  else {
+    for(i in 1:length(secuencia)) {
+      data <- pres[, secuencia2[i]:secuencia[i]]
+      data2 <- unmarkedFrameOccu(y = data, siteCovs = sitecov, obsCovs = obscov)
+      try({
+        #Partimos en dos Detección y occupancia
+        form <- as.character(form)
+        Div <- str_squish(form) %>% str_remove_all(" ")  %>% stringr::str_split(pattern = "~", simplify = T)
+        
+        ### Separamos dos formulas Occupancia y Deteccion
+        
+        Det <- Div[length(Div) - 1]
+        
+        VarDet <- str_split(Det, "\\+", simplify = T) %>% as.character()
+        
+        Fs <- list()
+        
+        for(x in 1:(length(VarDet) + 1)){
+          if(x == (length(VarDet) + 1)){
+            Formulas <- data.frame(Form = "~1 ~ 1", AICc = NA)
+            Formulas$AICc[j] <- try(MuMIn::AICc(occu(as.formula("~1 ~1"), data2)), silent = T)
+          }else{
+            Test <- combn(VarDet, x, simplify = F)
+            Formulas <- data.frame(Form = rep(NA, length(Test)), AICc = rep(NA, length(Test)))
+            for(j in 1:length(Test)){
+              Temp <- paste("~", paste(Test[[j]], collapse = " + "), "~ 1") 
+              Formulas$Form[j] <- Temp
+              Temp <- as.formula(Temp)
+              Formulas$AICc[j] <- try(MuMIn::AICc(occu(Temp, data2)), silent = T) 
+            }
+          }
+          
+          Fs[[x]] <- suppressWarnings(Formulas %>% mutate(AICc = as.numeric(AICc)) %>% dplyr::filter(!is.na(AICc)) %>% arrange(AICc))
+        }
+        
+        Fs <- suppressWarnings(purrr::reduce(Fs, bind_rows) %>% arrange(AICc))
+        
+        Selected <- Fs$Form[1] %>% str_split("~", simplify = T) %>% as.character()
+        Selected <- Selected[length(Selected) - 1] %>% str_squish()
+        
+        
+        Occup <- Div[length(Div)]
+        
+        VarOccup <- str_split(Occup, "\\+", simplify = T) %>% as.character()
+        
+        Fs <- list()
+        
+        for(x in 1:(length(VarOccup) + 1)){
+          if(x == (length(VarOccup) + 1)){
+            Formulas <- data.frame(Form = paste("~",Selected, "~ 1"), AICc = NA)
+            Formulas$AICc[j] <- try(MuMIn::AICc(occu(as.formula(paste("~",Selected, "~ 1")), data2)), silent = T)
+          }else{
+            Test <- combn(VarOccup, x, simplify = F)
+            Formulas <- data.frame(Form = rep(NA, length(Test)), AICc = rep(NA, length(Test)))
+            for(j in 1:length(Test)){
+              Temp <- paste("~", Selected, "~", paste(Test[[j]], collapse = " + ")) 
+              Formulas$Form[j] <- Temp
+              Temp <- as.formula(Temp)
+              Formulas$AICc[j] <- try(MuMIn::AICc(occu(Temp, data2)), silent = T) 
+            }
+          }
+          
+          Fs[[x]] <- suppressWarnings(Formulas %>% mutate(AICc = as.numeric(AICc)) %>% dplyr::filter(!is.na(AICc)) %>% arrange(AICc))
+        }
+        
+        Fs <- suppressWarnings(purrr::reduce(Fs, bind_rows) %>% arrange(AICc))
+        
+        Mods[[i]] <- Fs
+        
+        Best <- Fs$Form[1]
+        
+        models[[i]] <- occu(as.formula(Best), data2)
+        #dredged <- suppressWarnings(dredge(occu(form, data2)))
+        # select the first model and evaluate
+        #models[[i]] <- eval(getCall(dredged, 1))
+        
+      }, silent = T)
+      try({
+        #predictions for the best model
+        fit[, i] <- suppressWarnings(predict(models[[i]], type = "state", newdata = sitecov))$Predicted
+      }, silent = T)
+      
+      print(paste("Species", as.character(i), "ready!"))
+    }
+  }
+  if(is.null(SppNames)){
+    names(models) <- paste("species", 1:spp, sep =".")
+  }else if(class(SppNames) == "character"){
+    names(models) <- SppNames
+  }
+  
+  if(is.null(SppNames)){
+    names(Mods) <- paste("species", 1:spp, sep =".")
+  }else if(class(SppNames) == "character" & !is.null(Mods)){
+    names(Mods) <- SppNames
+  }
+  
+  cond <- sapply(models, function(x) !is.null(x))
+  models <- models[cond]
+  fit <- fit[,cond]
+  Not <- SppNames[!(cond)]
+  if(sum(!cond) >= 1){
+    message(paste("species", paste(Not, collapse = ", "), "did not converge, try with less variables"))
+  }
+  result <- list(Covs = sitecov, models = models, fit = fit, Mods = Mods)
+  class(result)<- "batchoccupancy"
+  return(result)
+}
+
+
+Spp <- data_reg %>% colnames() %>% str_remove_all("1")%>% str_remove_all("2") %>% str_remove_all("3") %>% unique()
+
+DetInv <- batchoccu2(pres = data_reg, sitecov = data_ocu, obscov = data_det, spp=45,  form= "~ Temperatura +Humedad+ DirViento +RapViento+ Agua ~ 1", dredge=TRUE, SppNames = Spp)
+
+Detecciones <- list()
+
+for(i in 1:length(DetInv$models)){
+  Detecciones[[i]] <- DetInv$models[[i]] %>% predict(type = "det") %>% pull(Predicted) %>% matrix(ncol = 3, byrow = T) %>% as.data.frame()
+  colnames(Detecciones[[i]]) <- paste0(names(DetInv$models)[i],"_",c("Dia1", "Dia2", "Dia3"))
+}
+
+
+
+Detecciones <- Detecciones %>% reduce(bind_cols)
+
+rownames(Detecciones) <- rownames(data_reg)
+
+### Correccion de abundancias
+
+AbundInv_dia <-read_rds("Occdata_regInv.rds")
+
+AbundInv_Corregido <- AbundInv_dia/Detecciones
+saveRDS(AbundInv_Corregido, "AbundInv_Corregido.rds")
