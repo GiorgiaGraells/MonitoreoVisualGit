0d73f53a14d588d6379473715a80e080a613e7bc
derek-corcoran-barrios <derek.corcoran.barrios@gmail.com>
2021-12-25 09:29:33 +0100

    Dos analisis buffer

--- Analisis_Occu_punto/Occu37sp.R
+++ Analisis_Occu_punto/Occu37sp.R
@@ -431,17 +431,10 @@ Todos <- list()
 
 Presences_by_sp <- list.files(path = "Final_Ocurrences/", pattern = ".rds", full.names = T)
 
-Nclust <- floor(parallel::detectCores()/4)
-cl <- makeSOCKcluster(Nclust)
-registerDoSNOW(cl)
 
-ntasks <- length(Presences_by_sp)
-pb <- tkProgressBar(max=ntasks)
-progress <- function(n) setTkProgressBar(pb, n)
-opts <- list(progress=progress)
+Todos <- list()
 
-
-Todos <- foreach(i = 1:length(Distancias), .packages = c("sf","dplyr", "stringr", "raster", "maxnet", "terra", "dismo", "magrittr"), .options.snow=opts, .combine = bind_rows()) %dopar% {
+for(i in 1:length(Distancias)){
   Temp <- Mods[[i]]
   PorSpp <- list()
   for(j in 1:length(SppNames)){
@@ -454,12 +447,175 @@ Todos <- foreach(i = 1:length(Distancias), .packages = c("sf","dplyr", "stringr"
     Temp3$AICc <- AICc(Temp2)
     PorSpp[[j]] <- Temp3
   }
-  PorSpp %>% purrr::reduce(bind_rows)
+  Todos[[i]] <- PorSpp %>% purrr::reduce(bind_rows)
   message(paste("lista distancia", Distancias[i]))
 }
 
+Todos <- Todos %>% purrr::reduce(bind_rows)
+
 write_csv(Todos, "Resumen_ocuInv_buffer.csv")
 
+Todos <-read_csv("Resumen_ocuInv_buffer.csv")
+
+Seleccionados <- Todos %>% 
+  dplyr::select(-Parametro, -Estimador) %>% 
+  group_by(Especies) %>% 
+  dplyr::filter(AICc == min(AICc)) %>% 
+  distinct() %>% 
+  ungroup() %>%
+  dplyr::group_by(Distancia) %>% 
+  summarise(n = n())
+
+
+ggplot(Seleccionados, aes(x = Distancia, y = n)) +
+  geom_path() +
+  theme_bw() 
+
+######## Con solo algunas variables
+
+# BUFFER 30 M DE LOS SITIOS DE MUESTREO
+OccuInv37_30 <- batchoccu2(pres = data_reg, sitecov = data_ocu, obscov = data_det, 
+                           spp=37 , 
+                           form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_30_Pastizales+Buffer_30_Matorrales+ Buffer_30_Sup_impermeables+ Buffer_30_Oceano+ Buffer_30_Suelo_arenoso+Buffer_30_Rocas+ Buffer_30_Grava", 
+                           dredge=TRUE,  
+                           SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL"))
+
+
+#BUFFER 600M DE LOS SITIOS DE MUESTREO
+OccuInv37_600 <- batchoccu2(pres = data_reg, 
+                            sitecov = data_ocu, 
+                            obscov = data_det, 
+                            spp=37 , 
+                            form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_600_Bosque_Nativo + Buffer_600_Pastizales+Buffer_600_Matorrales+ Buffer_600_Humedales+ Buffer_600_Sup_impermeables+ Buffer_600_Oceano+ Buffer_600_Suelo_arenoso+Buffer_600_Rocas+ Buffer_600_Grava", 
+                            dredge=TRUE,  
+                            SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL" ))
+
+#BUFFER 1100M DE LOS SITIOS DE MUESTREO
+OccuInv37_1100 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_1100_Bosque_Nativo + Buffer_1100_Pastizales+Buffer_1100_Matorrales+ Buffer_1100_Humedales+ Buffer_1100_Sup_impermeables+ Buffer_1100_Oceano+ Buffer_1100_Suelo_arenoso+Buffer_1100_Rocas+ Buffer_1100_Grava ", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL" ))
+
+#BUFFER 1700M DE LOS SITIOS DE MUESTREO
+OccuInv37_1700 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_1700_Bosque_Nativo+Buffer_1700_Pastizales+Buffer_1700_Matorrales+ Buffer_1700_Humedales+ Buffer_1700_Sup_impermeables+ Buffer_1700_Oceano+ Buffer_1700_Suelo_arenoso + Buffer_1700_Rocas+ Buffer_1700_Grava", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL"))
+
+#BUFFER 2200M DE LOS SITIOS DE MUESTREO
+OccuInv37_2200 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_2200_Bosque_Nativo+Buffer_2200_Pastizales+Buffer_2200_Matorrales+ Buffer_2200_Humedales+ Buffer_2200_Sup_impermeables+ Buffer_2200_Oceano+ Buffer_2200_Suelo_arenoso +Buffer_2200_Rocas+ Buffer_2200_Grava", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL"))
+
+#BUFFER 2800M DE LOS SITIOS DE MUESTREO
+OccuInv37_2800 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_2800_Bosque_Nativo + Buffer_2800_Pastizale s+Buffer_2800_Matorrales + Buffer_2800_Humedales+ Buffer_2800_Sup_impermeables + Buffer_2800_Oceano+ Buffer_2800_Suelo_arenoso + Buffer_2800_Rocas+ Buffer_2800_Grava", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL" ))
+
+#BUFFER 3300M DE LOS SITIOS DE MUESTREO
+OccuInv37_3300 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_3300_Bosque_Nativo+Buffer_3300_Pastizales+Buffer_3300_Matorrales+ Buffer_3300_Humedales+ Buffer_3300_Sup_impermeables+ Buffer_3300_Oceano+ Buffer_3300_Suelo_arenoso +Buffer_3300_Rocas+ Buffer_3300_Grava", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL" ))
+
+#BUFFER 3900M DE LOS SITIOS DE MUESTREO
+OccuInv37_3900 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_3900_Bosque_Nativo+Buffer_3900_Pastizales+Buffer_3900_Matorrales+ Buffer_3900_Humedales+ Buffer_3900_Sup_impermeables+ Buffer_3900_Oceano+ Buffer_3900_Suelo_arenoso + Buffer_3900_Rocas+ Buffer_3900_Grava", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL"))
+
+#BUFFER 4400M DE LOS SITIOS DE MUESTREO
+OccuInv37_4400 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_4400_Bosque_Nativo+Buffer_4400_Pastizales+Buffer_4400_Matorrales+ Buffer_4400_Humedales+ Buffer_4400_Sup_impermeables+ Buffer_4400_Oceano+ Buffer_4400_Suelo_arenoso + Buffer_4400_Rocas+ Buffer_4400_Grava", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL"))
+
+#BUFFER 50000M DE LOS SITIOS DE MUESTREO
+OccuInv37_5000 <- batchoccu2(pres = data_reg, 
+                             sitecov = data_ocu, 
+                             obscov = data_det, 
+                             spp=37 , 
+                             form= "~ Temperatura + Humedad + DirViento + RapViento + Agua ~ Buffer_5000_Bosque_Nativo+Buffer_5000_Pastizales+Buffer_5000_Matorrales+ Buffer_5000_Humedales+ Buffer_5000_Sup_impermeables+ Buffer_5000_Oceano+ Buffer_5000_Suelo_arenoso + Buffer_5000_Rocas+ Buffer_5000_Grava", 
+                             dredge=TRUE,  
+                             SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL" ))
+
+########################
+#para generar data frame con resumen de modelos para todas las especies y todos los buffers
+
+Distancias <- round(seq(from = 30, to = 5000, length.out = 10), -2)
+Distancias[1] <- 30
+SppNames = c("BLANQUILLO", "CHERCAN", "CHINCOL",  "CHURRETE_CHICO", "CHURRETE_COMUN", "CHURRETE_COSTERO", "COLEGIAL", "CORMORAN", "COTORRA_ARGENTINA","DIUCA", "GAVIOTA_FRANKLIN","GAVIOTA_GARUMA", "GAVIOTA", "GOLONDRINA_CHILENA",  "GOLONDRINA_DORSO_NEGRO",  "GORRION",  "GUANAY",  "HUAIRAVO",  "JOTE_CABEZA_NEGRA", "JOTE_CABEZA_ROJA", "LILE",  "MIRLO", "GAVIOTIN_MONJA",  "PALOMA", "PELICANO", "PERRITO",   "PILPILEN", "PILPILEN_NEGRO", "PIQUERO", "QUELTEHUE",  "RARA",   "TAGUA_COMUN", "TENCA",  "TIUQUE",  "TORDO",   "TORTOLA",  "ZORZAL" )
+
+Mods <- list(OccuInv37_30, OccuInv37_600, OccuInv37_1100, OccuInv37_1700, OccuInv37_2200, OccuInv37_2800, OccuInv37_3300, OccuInv37_3900, OccuInv37_4400, OccuInv37_5000)
+
+Todos <- list()
+
+Presences_by_sp <- list.files(path = "Final_Ocurrences/", pattern = ".rds", full.names = T)
+
+
+Todos <- list()
+
+for(i in 1:length(Distancias)){
+  Temp <- Mods[[i]]
+  PorSpp <- list()
+  for(j in 1:length(SppNames)){
+    Temp2 <- Temp$models[[SppNames[j]]]
+    Temp3 <- data.frame(Especies=rep(NA,length(Temp$models[[SppNames[j]]]@estimates@estimates$state@estimates)), Parametro=rep(NA,length(Temp$models[[SppNames[j]]]@estimates@estimates$state@estimates)), Estimador=rep(NA,length(Temp$models[[SppNames[j]]]@estimates@estimates$state@estimates)), SE=rep(NA,length(Temp$models[[SppNames[j]]]@estimates@estimates$state@estimates)), Distancia=rep(NA,length(Temp$models[[SppNames[j]]]@estimates@estimates$state@estimates)), p = NA, AICc = NA)
+    Temp3$Especies <- SppNames[j]
+    Temp3$Parametro <- Temp$models[[SppNames[j]]]@estimates@estimates$state@estimates %>% names() %>% str_remove_all(pattern = paste0("Buffer_",Distancias[i], "_"))
+    Temp3$Estimador <- Temp$models[[SppNames[j]]]@estimates@estimates$state@estimates %>% as.numeric() 
+    Temp3$Distancia <- Distancias[i]
+    Temp3$AICc <- AICc(Temp2)
+    PorSpp[[j]] <- Temp3
+  }
+  Todos[[i]] <- PorSpp %>% purrr::reduce(bind_rows)
+  message(paste("lista distancia", Distancias[i]))
+}
+
+Todos <- Todos %>% purrr::reduce(bind_rows)
+
+write_csv(Todos, "Resumen_ocuInvNuevo_buffer.csv")
+
+Todos <-read_csv("Resumen_ocuInvNuevo_buffer.csv")
+
+Seleccionados <- Todos %>% 
+  dplyr::select(-Parametro, -Estimador) %>% 
+  group_by(Especies) %>% 
+  dplyr::filter(AICc == min(AICc)) %>% 
+  distinct() %>% 
+  ungroup() %>%
+  dplyr::group_by(Distancia) %>% 
+  summarise(n = n())
+
+
+ggplot(Seleccionados, aes(x = Distancia, y = n)) +
+  geom_path() +
+  theme_bw() 
+
+#######
 
 #Grafico para todas las especies y variables x distancia
 library(ggplot2)
@@ -469,7 +625,7 @@ Todos <- Todos %>% mutate(Origen= case_when(Especies %in% c("BLANQUILLO","CHURRE
                                             Especies %in% c("CHERCAN","CHINCOL","COLEGIAL","COTORRA_ARGENTINA","DIUCA","GOLONDRINA_CHILENA","GOLONDRINA_DORSO_NEGRO", "GORRION","JOTE_CABEZA_NEGRA","JOTE_CABEZA_ROJA","MIRLO","PALOMA","RARA","TENCA","TIUQUE","TORDO","TORTOLA","ZORZAL")~"Terrestre"))
 
 ggplot(Todos, aes(x=Distancia, y=Estimador)) + geom_line(aes(color=Parametro)) + facet_wrap(~Especies, scales = "free_y")
-ggplot(Todos, aes(x=Distancia, y=Estimador, group=Especies)) + geom_line(aes(color=Origen)) + facet_wrap(~Parametro)
+ggplot(Todos, aes(x=Distancia, y=Estimador, group=Especies)) + geom_line(aes(color=Origen)) + facet_wrap(~Parametro, scales = "free")
 
 
 #ESPECIES MARINAS
